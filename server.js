5292e857c197df8c18bd851c13854f90bb954e49
Lauren Perez <30421262+laurenperez@users.noreply.github.com>
2022-05-24 20:27:18 -0700

    init commit

---
+++ server.js
@@ -0,0 +1,14 @@
+// Dependencies
+require("dotenv").config()
+const PORT = process.env.PORT || 3001
+const express = require("express")
+const app = express()
+
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello World")
+})
+
+// Listener
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
