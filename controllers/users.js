10f3610277ceb07eced0971abcbef9f056bbee83
Lauren Perez <30421262+laurenperez@users.noreply.github.com>
2022-06-03 21:33:28 -0700

    updates startercode

--- controllers/users.js
+++ controllers/users.js
@@ -1,18 +1,5 @@
 const User = require("../models/user");
 
-// Create
-// const signup = async (req, res) => {
-//   const user = new User(req.body);
-//   try {
-//     await user.save();
-//     // TODO: Send back a JWT instead of the user
-//     res.json(user);
-//   } catch (err) {
-//     // Probably a schema error
-//     res.status(400).json(err);
-//   }
-// };
-
 const signup = async (req, res) => {
   try {
     // TODO: Send back a JWT instead of the user
